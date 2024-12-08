a2977da6ecaa56832673f9cb0c63a3239d94fd60
Erick Muñoz <musal1985@gmail.com>
2024-12-08 11:52:40 -0400

    Reparación del código

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Publication
 # Create your views here.
 
 class HomePageView(ListView):
     model = Publication
     template_name = "publication_list.html"
+
+class PublicationCreateView(CreateView):
+    model = Publication
+    template_name = "publication_new.html"
+    fields = ["title", "author", "body"]
+
+class PublicationDetailView(DetailView):
+    model = Publication
+    template_name = "publication_deteail.html"
+
 
     
