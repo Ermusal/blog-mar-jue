cae071435db37d02c246bf6c7bf69c2365c4dd0b
Erick Muñoz <musal1985@gmail.com>
2024-12-12 17:28:44 -0400

    publication delete done

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import HomePageView, PublicationCreateView, PublicationDetailView, PublicationUpdateView
+from .views import (HomePageView, 
+                    PublicationCreateView, 
+                    PublicationDetailView, 
+                    PublicationUpdateView, 
+                    PublicationDeleteView,
+                    )
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('publication/new/', PublicationCreateView.as_view(), name='publication_new'),
     path('publication/<int:pk>/', PublicationDetailView.as_view(), name='publication_deteail'),
     path('publication/<int:pk>/update/', PublicationUpdateView.as_view(), name='publication_update'),
+    path('publication/<int:pk>/delete/', PublicationDeleteView.as_view(), name='publication_delete'),
 ]    
